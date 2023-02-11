2b4f54f7567cd6397d58134943ba9f07f111a0be
Sam.Donovan <samcdonovan@gmail.com>
2023-02-11 12:18:12 +0000

    Add text-based UI for controlling the API

--- src/main/java/samcdonovan/java/Main.java
+++ src/main/java/samcdonovan/java/Main.java
@@ -1,7 +1,30 @@
 package samcdonovan.java;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Frog Bookstore API!");
+        System.out.println("Do you want to get or set a person's info?");
+        System.out.println("(1) Create");
+        System.out.println("(2) Read");
+        System.out.println("(3) Update");
+        System.out.println("(4) Delete");
+        System.out.println("(Type 'get' or 'set' now.)");
+        String userInput = scanner.nextLine();
+
+        switch(userInput) {
+            case ("1"):
+            case ("2"):
+            case ("3"):
+            case ("4"):
+            default:
+                System.out.println(userInput + " is not a valid option.");
+
+        }
+        scanner.close();
+
     }
 }
