5f71cc99e9dedbee35fedc7d004c0fe064af06c8
Sam.Donovan <samcdonovan@gmail.com>
2023-02-12 16:52:40 +0000

    Add schema and data files for H2 database initialisation

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO BOOKS (title, author, isbn, price) VALUES
+  ('The Fellowship Of The Ring', 'J.R.R. Tolkien', '9780261103573', 6.5),
+  ('The Two Towers', 'J.R.R. Tolkien', '9780261103580', 6.5),
+  ('Return Of The King', 'J.R.R. Tolkien', '9780261103587', 6.5);
