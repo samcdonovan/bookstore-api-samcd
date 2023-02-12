3d257d7c9e729ab82a6c68143d5fd8dbb29ba6e5
Sam.Donovan <samcdonovan@gmail.com>
2023-02-12 16:58:45 +0000

    Delete AppConfig

--- src/main/java/samcdonovan/java/AppConfig.java
+++
@@ -1,8 +0,0 @@
-package samcdonovan.java;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-@Configuration
-public class AppConfig {
-
-}
