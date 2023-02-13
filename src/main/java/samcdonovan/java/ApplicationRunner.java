ef82dd80b0d669a977e506054385a355f8bd0948
Sam.Donovan <samcdonovan@gmail.com>
2023-02-13 20:07:39 +0000

    Add welcome message after Spring app is running

--- src/main/java/samcdonovan/java/ApplicationRunner.java
+++ src/main/java/samcdonovan/java/ApplicationRunner.java
@@ -2,6 +2,8 @@ package samcdonovan.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class ApplicationRunner {
@@ -9,5 +11,15 @@ public class ApplicationRunner {
 
         SpringApplication.run(ApplicationRunner.class, args);
 
+    }
+
+    /**
+     * Helper function to print a message once the Spring
+     * application is running
+     */
+    @EventListener(ApplicationReadyEvent.class)
+    public void appReadyMessage() {
+        System.out.println("\nApplication has started at:");
+        System.out.println("localhost:8080/books\n");
     }
 }
