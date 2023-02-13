4a7fbc6e6bf6760074bd227e75c36f63390f6a8a
Sam.Donovan <samcdonovan@gmail.com>
2023-02-13 19:50:50 +0000

    Fix initial SQL data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,5 +4,5 @@ INSERT INTO books (title, author, isbn, price) VALUES
   ('1984', 'George Orwell', '9780261103569', 5.0),
   ('The Two Towers', 'J.R.R. Tolkien', '9780261103580', 6.5),
   ('Coraline', 'Neil Gaiman', '9780261103554', 8.0),
-  ('Return Of The King', 'J.R.R. Tolkien', '9780261103587', 6.5);
-  ('Animal Farm', 'George Orwell', '9780261103570', 4.5),
+  ('Return Of The King', 'J.R.R. Tolkien', '9780261103587', 6.5),
+  ('Animal Farm', 'George Orwell', '9780261103570', 4.5);
