c5bc0baaee99cfe801df64a78ff7d264ece75af3
Sam.Donovan <samcdonovan@gmail.com>
2023-02-13 16:48:15 +0000

    Add more data to data.sql for database initialisation

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,8 @@
 --INSERT starting data into the books table
 INSERT INTO books (title, author, isbn, price) VALUES
   ('The Fellowship Of The Ring', 'J.R.R. Tolkien', '9780261103573', 6.5),
+  ('1984', 'George Orwell', '9780261103569', 5.0),
   ('The Two Towers', 'J.R.R. Tolkien', '9780261103580', 6.5),
+  ('Coraline', 'Neil Gaiman', '9780261103554', 8.0),
   ('Return Of The King', 'J.R.R. Tolkien', '9780261103587', 6.5);
+  ('Animal Farm', 'George Orwell', '9780261103570', 4.5),
