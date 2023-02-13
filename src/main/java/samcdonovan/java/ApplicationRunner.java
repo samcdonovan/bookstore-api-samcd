b2828d3a9b167d0b48975b9911d874df367b34c9
Sam.Donovan <samcdonovan@gmail.com>
2023-02-13 11:00:31 +0000

    Remove text-based interface from main function

--- src/main/java/samcdonovan/java/ApplicationRunner.java
+++ src/main/java/samcdonovan/java/ApplicationRunner.java
@@ -8,29 +8,6 @@ public class ApplicationRunner {
     public static void main(String[] args) {
 
         SpringApplication.run(ApplicationRunner.class, args);
-
-        /*
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Welcome to the Frog Bookstore API!");
-        System.out.println("Do you want to get or set a person's info?");
-        System.out.println("(1) Create");
-        System.out.println("(2) Read");
-        System.out.println("(3) Update");
-        System.out.println("(4) Delete");
-        System.out.println("(Type 'get' or 'set' now.)");
-        String userInput = scanner.nextLine();
-
-        switch(userInput) {
-            case ("1"):
-            case ("2"):
-            case ("3"):
-            case ("4"):
-            default:
-                System.out.println(userInput + " is not a valid option.");
-
-        }
-        scanner.close();
-      */
+        
     }
 }
