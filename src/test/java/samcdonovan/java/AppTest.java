bfda620958d10dfe7e2724202eadd5a96918aad5
Sam.Donovan <samcdonovan@gmail.com>
2023-02-13 20:40:12 +0000

    Add BookController test

--- src/test/java/samcdonovan/java/AppTest.java
+++ src/test/java/samcdonovan/java/AppTest.java
@@ -1,4 +1,19 @@
 package samcdonovan.java;
 
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import samcdonovan.java.controller.BookController;
+
+@SpringBootTest
 public class AppTest {
+
+    @Autowired
+    private BookController bookController;
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(bookController).isNotNull();
+    }
 }
